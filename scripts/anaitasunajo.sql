c3650c0c735b695fc67ebfe2f10ff9630b870555
Julen <julenoi95@gmail.com>
2020-06-09 21:16:41 +0200

    Exportada la base de datos

--- scripts/anaitasunajo.sql
+++ scripts/anaitasunajo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-05-2020 a las 21:50:40
+-- Tiempo de generación: 09-06-2020 a las 21:15:52
 -- Versión del servidor: 10.4.6-MariaDB
 -- Versión de PHP: 7.2.22
 
@@ -25,23 +25,52 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `noticias`
+-- Estructura de tabla para la tabla `contacto`
 --
 
-CREATE TABLE `noticias` (
-  `ID` int(11) NOT NULL,
-  `Título` varchar(60) NOT NULL,
-  `Descripción` text NOT NULL,
-  `Imagen` text NOT NULL
+CREATE TABLE `contacto` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellidos` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `mensaje` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Volcado de datos para la tabla `noticias`
+-- Volcado de datos para la tabla `contacto`
 --
 
-INSERT INTO `noticias` (`ID`, `Título`, `Descripción`, `Imagen`) VALUES
-(1, 'Reapertura de las piscinas exteriores', 'Desde este próximo fin de semana, las piscinas exteriores se reabrirán para el público hasta el final de la temporada de verano.\r\n\r\nLes informamos que deben de respetar a los socorristas y cuidar el espacio abierto para el disfrute de todos.\r\n\r\nAdemás, el día de la inauguración a las 16:00, habrá preparado un evento para los niños con un hinchables y juegos variados', ''),
-(2, 'Ven a apoyar a nuestro equipo de balonmano', 'El proximo día 25 de mayo a 12:00, nuestro equipo de balonmano, SCR Anaitasuna se juega el pase a la liga de campeones del próximo año contra el Rios Renovables.\r\n\r\nLos precios de las entradas son los siguientes:\r\n\r\n- 1 adulto: 8€ (antes 12€)\r\n- 2 adultos: 15€ (antes 20€)\r\n\r\nLas entradas de los niños a partir de 8 años serán de 1€ por cada niño acompañado de un adulto responsable.\r\n\r\nLlenemos el pabellón y seamos el jugador extra para ayudar a nuestro equipo favorito, la apertura de puertas será a las 11:00', '');
+INSERT INTO `contacto` (`id`, `nombre`, `apellidos`, `email`, `mensaje`) VALUES
+(5, 'Julen', 'Osia IbÃ¡Ã±ez', 'julenoi95@gmail.com', 'Hola buenas vengo a hacer una prueba con AngularJS'),
+(6, 'Pepe', 'Perez', 'pepep43@gmail.com', 'Hola esto es una prueba para la base'),
+(7, 'Pepe', 'Perez', 'pepep43@gmail.com', 'Hola, esto es una prueba para el funcionamiento del formulario');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `deportes`
+--
+
+CREATE TABLE `deportes` (
+  `iddeporte` int(11) NOT NULL,
+  `deporte` varchar(50) NOT NULL,
+  `maxjugadores` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `deportes`
+--
+
+INSERT INTO `deportes` (`iddeporte`, `deporte`, `maxjugadores`) VALUES
+(1, 'balonmano', 50),
+(2, 'gimnasia', 10),
+(3, 'montañismo', 30),
+(4, 'ciclismo', 9),
+(5, 'futbol', 16),
+(6, 'halterofilia', 7),
+(7, 'judo', 10),
+(8, 'natacion', 21),
+(9, 'padel', 5);
 
 -- --------------------------------------------------------
 
@@ -50,45 +79,71 @@ INSERT INTO `noticias` (`ID`, `Título`, `Descripción`, `Imagen`) VALUES
 --
 
 CREATE TABLE `socios` (
-  `DNI` varchar(9) NOT NULL,
-  `Nombre` varchar(50) NOT NULL,
-  `Apellidos` varchar(100) NOT NULL,
-  `Correo` varchar(100) NOT NULL,
-  `Telefono` int(9) NOT NULL
+  `dni` varchar(9) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(100) NOT NULL,
+  `edad` int(2) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `iddeporte` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `socios`
 --
 
-INSERT INTO `socios` (`DNI`, `Nombre`, `Apellidos`, `Correo`, `Telefono`) VALUES
-('73589639F', 'Pablo', 'García López', 'pGarcíaLo@gmail.com', 693489962);
+INSERT INTO `socios` (`dni`, `nombre`, `apellido`, `edad`, `email`, `iddeporte`) VALUES
+('12341241G', 'Pepe', 'Perez', 36, 'pepep43@gmail.com', 1),
+('19599897G', 'Sonia', 'Luna', 25, 'sonilu95@gmail.com', 2),
+('73430149G', 'Julen', 'Osia', 25, 'julenoi95@gmail.com', 1);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `noticias`
+-- Indices de la tabla `contacto`
 --
-ALTER TABLE `noticias`
-  ADD PRIMARY KEY (`ID`);
+ALTER TABLE `contacto`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `deportes`
+--
+ALTER TABLE `deportes`
+  ADD PRIMARY KEY (`iddeporte`);
 
 --
 -- Indices de la tabla `socios`
 --
 ALTER TABLE `socios`
-  ADD PRIMARY KEY (`DNI`);
+  ADD PRIMARY KEY (`dni`),
+  ADD KEY `iddeporte` (`iddeporte`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `noticias`
+-- AUTO_INCREMENT de la tabla `contacto`
 --
-ALTER TABLE `noticias`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `contacto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `deportes`
+--
+ALTER TABLE `deportes`
+  MODIFY `iddeporte` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `socios`
+--
+ALTER TABLE `socios`
+  ADD CONSTRAINT `socios_ibfk_1` FOREIGN KEY (`iddeporte`) REFERENCES `deportes` (`iddeporte`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
